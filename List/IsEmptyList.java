fa65bec74fef971c572ecf1a375431dbb58c6d2d
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2026-04-03 22:47:57 +0530

    Program to check the list is empty or not

---
+++ List/IsEmptyList.java
@@ -0,0 +1,12 @@
+/*
+    Program to check the list is empty or not: 
+        isEmpty() method returns true if the list doesn't contains elements else returns false
+*/
+
+import java.util.ArrayList;
+public class IsEmptyList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        System.out.print(arr.isEmpty());
+    }
+}
